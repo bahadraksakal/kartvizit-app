99197a3850fbe3ed86c324604135a3063e3f723a
Bahadır Aksakal <72493865+bahadraksakal@users.noreply.github.com>
2023-07-20 11:48:27 +0300

    add config
    add config

---
+++ kartvizit-backend-app/app.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    connectionString: "mongodb+srv://appkartvizit:HX0XN81eKuNfwiUZ@cluster0.g5x3dtg.mongodb.net/kartvizit-app?retryWrites=true&w=majority",
+    secureKey: "7837280274ed9603408956551fb5c7408cbabf1510a4762e4482b981805a050b"
+  };
